3ab1d42aed45459aa58d6b915d48d12bf70deb6f
User <lonzo.trans@gmail.com>
2023-02-27 14:57:03 +0300

    change 26/02

--- main/urls.py
+++ main/urls.py
@@ -7,4 +7,10 @@ urlpatterns = [
     path('', views.index),
     path('about/', views.about),
     path('create/', views.create),
+    path('addtask/', views.add_task),
+    path('edittask/', views.edit_task, name='edittask'),
+    path('<int: pk>/update', views.TaskUpdateView. as_view(), name='task_update'),
+    path('<int: pk>/delete', views.TaskDeleteView. as_view(), name='task_delete'),
+    path('<int: pk>', views.TaskDetailView. as_view(), name='task_detail'),
+
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
 from .forms import TaskForm
+from django.views.generic import DeleteView, DetailView, UpdateView
+
+class TaskDetailView(DetailView):
+    model = Task
+    template_name = 'detail_view.html'
+    context_object_name = 'article'
+
+
+class TaskUpdateView(UpdateView):
+    model = Task
+    template_name = 'create.html'
+    form_class = TaskForm
+
+
+class TaskDeleteView(DeleteView):
+    model = Task
+    success_url = 'task_detail'
+    template_name = 'deletetask.html'
+
 
 def index(request):
     tasks = Task.objects.all()
@@ -9,13 +28,26 @@ def index(request):
 def about(request):
     return render(request, 'main/about.html')
 
+def add_task(request):
+    return render(request, 'main/addtask.html')
+
+def edit_task(request):
+    tasks = Task.objects.all()
+    return render(request, 'main/edittask.html', {'tasks': tasks})
+
 def create(request):
-    if request.method == 'post':
+    error = ''
+    if request.method == 'POST':
         form = TaskForm(request.POST)
         if form.is_valid():
             form.save()
+            return redirect('/edittask')
+        else:
+            error = 'Форма заполнена некорректно!'
     form = TaskForm()
     context = {
-        'form': form
+        'form': form,
+        'error': error
+
     }
     return render(request, 'main/create.html', context)
